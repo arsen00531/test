ea31e037bcbff0a9af6324f5ee31f29b79253546
arsen00531 <arsendag505@gmail.com>
2024-03-24 17:12:18 +0300

    add token test

--- src/token/token.service.spec.ts
+++ src/token/token.service.spec.ts
@@ -1,12 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TokenService } from './token.service';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('TokenService', () => {
   let service: TokenService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TokenService],
+      providers: [
+        TokenService,
+        {
+          provide: ConfigService,
+          useValue: {
+            get: (key: string) => {
+              switch (key) {
+                case 'SECRET_OR_KEY':
+                  return 'secretKey';
+                case 'EXPIRED_JWT':
+                  return '1d';
+                default:
+                  return '';
+              }
+            },
+          },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<TokenService>(TokenService);
@@ -14,5 +39,11 @@ describe('TokenService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should generate a jwt token', () => {
+    const payload = { username: 'test' };
+    const token = service.generateJwtToken(payload);
+    expect(token).toBeDefined();
   });
 });
