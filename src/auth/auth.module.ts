7c2c910c23ba7a676f7ffaab37a6542c80b5cbce
arsen00531 <arsendag505@gmail.com>
2024-03-24 21:08:17 +0300

    fixes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtGuard } from './guards/jwt.guard';
-import { JwtStrategy } from 'src/strategy/jwt.strategy';
+import { JwtStrategy } from 'src/auth/strategy/jwt.strategy';
 import { UserModule } from 'src/user/user.module';
 import { TokenModule } from 'src/token/token.module';
 
